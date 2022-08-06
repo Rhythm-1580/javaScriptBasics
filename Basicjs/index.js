13d7c7b6b4ba76f3bde970a8db70ff953f26e712
Rhythm-1580 <rhythm3580.be20@chitkara.edu.in>
2022-08-06 19:45:42 +0530

    here we are

--- Basicjs/index.js
+++ Basicjs/index.js
@@ -1,11 +1,26 @@
 console.log("hello world");
 
 
-// syntax of function
-function sample() {
-  var a=34;
-  var b=3;
-  var c=a*b;
-  return c;
-}
-console.log(sample());
+        // syntax of function
+
+// function sample() {
+//   var a=34;
+//   var b=3;
+//   var c=a*b;
+//   return c;
+// }
+// console.log(sample());
+
+        //syntax of objects
+
+// var car={
+//   name: "Rhythm",
+//   grade:"a+",
+//   branch:"Cse",
+//   hostel:"ibn"
+// }
+// console.log(car);
+//
+      //if u wonna to print only name
+// console.log(car.name);
+//
